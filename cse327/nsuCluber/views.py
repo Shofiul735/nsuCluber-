5ced5b0f725b2bedb0c817f8c09d20c3cfb12dfc
shofiul735 <ytshofiul@gamil.com>
2020-03-25 22:09:24 +0600

    home page design finished

--- cse327/nsuCluber/views.py
+++ cse327/nsuCluber/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    '''
+      This function is use to display home page of our website and retrive some some data from database
+    '''
+    context = {
+    
+    }
+    return render(request,'index.html',context)
