97d268ca05772005d611955607d226093adf7728
abrarraiyan <61202230+abrarraiyan@users.noreply.github.com>
2020-03-26 15:32:32 +0600

    Add files via upload

---
+++ Cse327/nsuCluber/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    '''
+      This function is use to display reservation page of our website and retrive some some data from database
+    '''
+    context = {
+    
+    }
+    return render(request,'reservation.html',context)
