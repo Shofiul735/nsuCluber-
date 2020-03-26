1631f5e851866fd99d4fa8c8d0c03d464c1f43c3
abrarraiyan <61202230+abrarraiyan@users.noreply.github.com>
2020-03-26 15:57:49 +0600

    Update views.py

--- Cse327/nsuCluber/views.py
+++ Cse327/nsuCluber/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def reservation(request):
     '''
       This function is use to display reservation page of our website and retrive some some data from database
     '''
