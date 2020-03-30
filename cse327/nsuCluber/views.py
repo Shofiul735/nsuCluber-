fbd2a02e9814de5241f1b873377d06b234f562bd
shofiul735 <ytshofiul@gamil.com>
2020-03-30 23:21:38 +0600

    adminbash board linked up with navbar and urls.py

--- cse327/nsuCluber/views.py
+++ cse327/nsuCluber/views.py
@@ -3,23 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    '''
-      This function is use to display home page of our website and retrive some some data from database
-    '''
+
+     # This function is use to display home page of our website and retrive some some data from database
+
     context = {
-     '''
-      for retriving database data
-     '''
+      #this is for database
     }
     return render(request,'index.html',context)
 
 def adminDash(request):
-    '''
-     This function is for admin Dashboard
-    '''
+
+     #This function is for admin Dashboard
+
     context = {
-     '''
-      for retriving database data
-     '''
+     # this is for database
     }
     return render(request,'includes/admin-dash.html',context)
