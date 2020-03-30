79bffd9c13a36186cb4da44dfcb22a2daa3f660a
shofiul735 <ytshofiul@gamil.com>
2020-03-30 22:35:43 +0600

    Viws is created for admin dashboard

--- cse327/nsuCluber/views.py
+++ cse327/nsuCluber/views.py
@@ -7,6 +7,19 @@ def index(request):
       This function is use to display home page of our website and retrive some some data from database
     '''
     context = {
-    
+     '''
+      for retriving database data
+     '''
     }
     return render(request,'index.html',context)
+
+def adminDash(request):
+    '''
+     This function is for admin Dashboard
+    '''
+    context = {
+     '''
+      for retriving database data
+     '''
+    }
+    return render(request,'includes/admin-dash.html',context)
