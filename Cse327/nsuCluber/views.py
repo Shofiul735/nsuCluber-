e6fc4af1f1cee972b260eb36dc70d4528dd596df
abrarraiyan <61202230+abrarraiyan@users.noreply.github.com>
2020-04-08 20:14:43 +0600

    Update views.py

--- Cse327/nsuCluber/views.py
+++ Cse327/nsuCluber/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def reservation(request):
-    '''
-      This function is use to display reservation page of our website and retrive some some data from database
-    '''
+def index(request):
+
+     # This function is use to display home page of our website and retrive some some data from database
+
     context = {
-    
+      #this is for database
+    }
+    return render(request,'index.html',context)
+
+def adminDash(request):
+
+     #This function is for admin Dashboard
+
+    context = {
+     # this is for database
+    }
+    return render(request,'includes/admin-dash.html',context)
+
+def reservation(request):
+
+     #This function is for admin Dashboard
+
+    context = {
+     # this is for database
     }
     return render(request,'reservation.html',context)
