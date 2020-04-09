ec18ca198ae72407090e46eb6b3ed33bf80caa83
shofiul735 <ytshofiul@gamil.com>
2020-04-09 23:17:20 +0600

    existingMember view added to the urls.py file

--- cse327/nsuCluber/urls.py
+++ cse327/nsuCluber/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='home'),
-    path('admin-dash',views.adminDash,name='admin')
+    path('admin-dash/',views.adminDash,name='admin'),
+    path('admin-dash/existing-member/',views.existingMember,name = "existingMember"),
 ]
