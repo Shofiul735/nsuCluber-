cc7493653e1067abf42a0ad1457c0334824f3409
shofiul735 <ytshofiul@gamil.com>
2020-05-01 21:37:07 +0600

    Admins datatable created

--- cse327/nsuCluber/models.py
+++ cse327/nsuCluber/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class admins(models.Model):
+    club_name = models.CharField(max_length = 20,primary_key = True)
+    password = models.CharField(max_length = 30 )
