9903817583bd1516b8317746c494591800459f80
shofiul735 <ytshofiul@gamil.com>
2020-05-01 22:10:08 +0600

    ExistingMember table register in our admin site

--- cse327/nsuCluber/admin.py
+++ cse327/nsuCluber/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AllAdmins
+from .models import AllAdmins,ExistingMember
 # Register your models here.
 
-admin.site.register(AllAdmins)
+admin.site.register(AllAdmins,ExistingMember)
