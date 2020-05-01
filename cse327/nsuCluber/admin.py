6ae063a04fa8331d9867d69852d52d050330582c
shofiul735 <ytshofiul@gamil.com>
2020-05-01 22:14:58 +0600

    database migrated

--- cse327/nsuCluber/admin.py
+++ cse327/nsuCluber/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import AllAdmins,ExistingMember
 # Register your models here.
 
-admin.site.register(AllAdmins,ExistingMember)
+admin.site.register(AllAdmins)
+admin.site.register(ExistingMember)
