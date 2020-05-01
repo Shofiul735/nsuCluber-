ad0fefc891b8a930c7ec341519121c2fa367844c
shofiul735 <ytshofiul@gamil.com>
2020-05-01 22:09:16 +0600

    ExistingMember table added in our model.py

--- cse327/nsuCluber/models.py
+++ cse327/nsuCluber/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class AllAdmins(models.Model):
     club_name = models.CharField(max_length = 20,primary_key = True)
     password = models.CharField(max_length = 30 )
+    def __str__(self):
+        return self.club_name
+    
+class ExistingMember(models.Model):
+    nsu_id = models.IntegerField()
+    name = models.CharField(max_length = 30)
+    club_name = models.ForeignKey(AllAdmins,on_delete=models.CASCADE)
+    position = models.CharField(max_length = 10)
+    def __str__(self):
+        return self.name
