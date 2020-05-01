b8d0c5978b9f88502ae77136de74ff13f0369f83
shofiul735 <ytshofiul@gamil.com>
2020-05-01 21:51:40 +0600

    AllAdmins model register to admin.py

--- cse327/nsuCluber/models.py
+++ cse327/nsuCluber/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class admins(models.Model):
+class AllAdmins(models.Model):
     club_name = models.CharField(max_length = 20,primary_key = True)
     password = models.CharField(max_length = 30 )
