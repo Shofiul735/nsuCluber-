ea5a937b514d4d9238485a43f76acd0d84090b42
shofiul735 <ytshofiul@gamil.com>
2020-05-02 12:04:36 +0600

    eventPost view added to urls.py

--- cse327/nsuCluber/urls.py
+++ cse327/nsuCluber/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.index, name='home'),
     path('admin-dash/',views.adminDash,name='admin'),
     path('admin-dash/existing-member/',views.existingMember,name = "existingMember"),
+    path('admin-dash/event-post',views.eventPost,name = "eventPost")
 ]
