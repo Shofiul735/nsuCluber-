fe9f5fcaf868fccfc8b0c2a42dae5f894881c659
shofiul735 <ytshofiul@gamil.com>
2020-05-03 12:19:28 +0600

    EventPost model registered in admin site

--- cse327/nsuCluber/admin.py
+++ cse327/nsuCluber/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AllAdmins,ExistingMember
+from .models import AllAdmins,ExistingMember,EventPost
 # Register your models here.
 
 admin.site.register(AllAdmins)
 admin.site.register(ExistingMember)
+admin.site.register(EventPost)
