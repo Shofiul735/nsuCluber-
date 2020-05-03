2206c329ebdc558193c84f2772ce54250b59ee43
shofiul735 <ytshofiul@gamil.com>
2020-05-03 12:22:59 +0600

    __str__ function added in EventPost model

--- cse327/nsuCluber/models.py
+++ cse327/nsuCluber/models.py
@@ -25,3 +25,6 @@ class EventPost(models.Model):
     eventDetails = models.TextField()
     keyWord = models.CharField(max_length = 50)
     
+    def __str__(self):
+        return self.title
+    
